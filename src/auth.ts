6f1a378d1fb207d987d7149885e2d891baa718fb
Jaime Cervantes Velasco <jaime.cervantes.ve@gmail.com>
2024-08-29 20:45:32 -0600

    refactor: rename google provider creator and update next auth

--- src/auth.ts
+++ src/auth.ts
@@ -2,15 +2,37 @@ import NextAuth from "next-auth";
 import type { NextAuthConfig } from "next-auth";
 import { FirestoreAdapter } from "@auth/firebase-adapter";
 
-import Google from "next-auth/providers/google";
+import createGoogleProvider from "next-auth/providers/google";
 
 export const config = {
   theme: {
     logo: "/logo.png",
   },
   adapter: FirestoreAdapter(),
-  providers: [Google],
+  providers: [createGoogleProvider],
+  callbacks: {
+    signIn(params) {
+      console.log(params);
+      return true;
+    },
+  },
   basePath: process.env.CJ_AUTH_PATH,
+  debug: process.env.NODE_ENV !== "production" ? true : false,
+  logger: {
+    error(error: Error) {
+      console.error("NextAuth error:", error.message, error.stack);
+    },
+    warn(code) {
+      console.warn(`NextAuth warning: ${code}`);
+    },
+    info(message: string) {
+      console.info("NextAuth info:", message);
+    },
+    debug(message: string, metadata: unknown) {
+      console.debug("NextAuth debug:", message);
+      console.debug("NextAuth debug:", metadata);
+    },
+  },
 } satisfies NextAuthConfig;
 
 export const { handlers, auth, signIn, signOut } = NextAuth(config);
