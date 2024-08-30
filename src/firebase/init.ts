30dc82e3f5e6ee487fe1afbd9cee334ee1c6baa9
Jaime Cervantes Velasco <jaime.cervantes.ve@gmail.com>
2024-08-29 22:22:40 -0600

    fix: return a strin type when there is no env.FIREBASE_SERVICE_ACCOUNT

--- src/firebase/init.ts
+++ src/firebase/init.ts
@@ -3,7 +3,7 @@ import admin from "firebase-admin";
 if (!admin.apps.length) {
   try {
 
-    const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT || {})
+    const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT || "{}")
     
     admin.initializeApp({
       credential: admin.credential.cert(serviceAccount),
