045dbe877ac64e5b897637ae701d1b3184cf54b9
Jaime Cervantes Velasco <jaime.cervantes.ve@gmail.com>
2024-09-15 19:52:43 -0600

    refactor: separate functions about posts model

--- src/firebase/models/posts.ts
+++ src/firebase/models/posts.ts
@@ -5,13 +5,7 @@ import type {
 import type { FirestorePost } from "./Posts.d";
 import type { Post, PostUser } from "~/types/Posts.d";
 import { Timestamp } from "firebase-admin/firestore";
-import invariant from "tiny-invariant";
-import { getStorage, getDownloadURL } from "firebase-admin/storage";
-import { getCollectionWithConverter } from "./converter";
-
-const collections = {
-  posts: () => getCollectionWithConverter<FirestorePost>("posts"),
-};
+import { createImageInStorage, validateFields, collections, defineSlug} from "./postUtils"
 
 const firstPage = 1;
 export async function getPosts(
@@ -65,74 +59,12 @@ export async function createPost(postInfo: Post, image: File, user: PostUser) {
 
     return { id: post.id, slug };
   } catch (error: any) {
+    console.log(error);
     return {
       error,
       errorMessage: "Algo salio mal al crear el post",
     };
   }
-}
-
-function validateFields(postInfo: Post, image: File, user: PostUser) {
-  invariant(
-    postInfo?.constructor === {}.constructor,
-    `"postInfo" debe ser un objeto, no un ${postInfo && postInfo?.constructor}`
-  );
-
-  if (image) {
-    invariant(
-      image?.constructor.name === "File",
-      `"image" debe ser un File, no un ${image && image?.constructor}`
-    );
-  }
-
-  invariant(
-    user?.constructor === {}.constructor,
-    `"user" debe ser un objeto, no un ${user && user?.constructor}`
-  );
-
-  invariant(postInfo.title, "El título es requerido");
-  invariant(postInfo.content, "El contenido es requerido");
-}
-
-async function defineSlug(title: string, slug?: string): Promise<string> {
-  if (!slug || slug?.trim() === "") {
-    slug = await createSlug(title, collections.posts());
-  }
-
-  return slug;
-}
-
-export async function createSlug(
-  title: string,
-  collection: CollectionReference<FirestorePost> = collections.posts()
-) {
-  const slug = title
-    .toLowerCase()
-    .normalize("NFD") // Normal Form Decomposition, convierte un character en dos o más, por ejemplo, su forma base y su acento
-    .replace(/[\u0300-\u036f]/g, "") // Remueve los acentos de las letras (diacríticos)
-    .replace(/[^a-z0-9]+/g, "-") // Remueve los carácteres que no sean letras o números, (incluyendo acentos)
-    .replace(/(^-|-$)+/g, ""); // Remueve los guiones al inicio y al final
-
-  const doc = await collection.where("slug", "==", slug).get();
-
-  if (doc.size) {
-    return `${slug}-${doc.size}`;
-  }
-
-  return slug;
-}
-
-async function createImageInStorage(image: File): Promise<string> {
-  const bucket = getStorage().bucket();
-  const buffer = Buffer.from(await image.arrayBuffer());
-
-  await bucket
-    .file(`posts/${image.name}`)
-    .save(buffer, { contentType: image.type });
-
-  const imageUrl = await getDownloadURL(bucket.file(`posts/${image.name}`));
-
-  return imageUrl;
 }
 
 export async function getPost(
