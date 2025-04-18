f963b6560616f43c72bf21ceda81b40f595c88e8
Jaime Cervantes <jaime.cervantes@s2g.energy>
2025-04-18 17:46:51 -0600

    test: add tests for ImageVideoPicker component

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,8 +2,8 @@ import type { StorybookConfig } from "@storybook/nextjs";
 
 const config: StorybookConfig = {
   stories: [
-    "../src/stories/**/*.mdx",
-    "../src/stories/**/*.stories.@(js|jsx|mjs|ts|tsx)",
+    "../src/**/*.mdx",
+    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)",
   ],
   addons: [
     "@storybook/addon-links",
@@ -14,8 +14,6 @@ const config: StorybookConfig = {
     name: "@storybook/nextjs",
     options: {},
   },
-  docs: {
-    autodocs: "tag",
-  },
+  docs: {},
 };
 export default config;
