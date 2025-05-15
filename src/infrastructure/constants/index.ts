7838518fa7ce68d5e8d67ce49bb825075867f744
Jaime Cervantes <jaime.cervantes.ve@gmail.com>
2025-05-15 14:18:19 -0600

    refactor: fix public env vars for pagination

--- src/infrastructure/constants/index.ts
+++ src/infrastructure/constants/index.ts
@@ -1,6 +1,6 @@
 export const SIGNIN_PATH = process.env.CJ_AUTH_PATH + '/signin';
-export const PAGINATION_INIT_PAGE = Number(process.env.PAGINATION_INIT_PAGE) || 1;
-export const PAGINATION_PAGE_SIZE = Number(process.env.PAGINATION_PAGE_SIZE) || 4;
-export const CANONICAL_URL = process.env.CANONICAL_URL || 'https://saludjusta.site'
+export const PAGINATION_INIT_PAGE = Number(process.env.NEXT_PUBLIC_PAGINATION_INIT_PAGE) || 1;
+export const PAGINATION_PAGE_SIZE = Number(process.env.NEXT_PUBLIC_PAGINATION_PAGE_SIZE) || 4;
+export const CANONICAL_URL = process.env.NEXT_PUBLIC_CANONICAL_URL || 'https://saludjusta.site'
 export const PUBLIC_BASE_URL = process.env.NEXT_PUBLIC_BASE_URL || "https://saludjusta.site"
 export const POST_CONTENT_MAX_LENGTH = process.env.NEXT_POST_CONTENT_MAX_LENGTH || 2500
