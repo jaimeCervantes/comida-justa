220208f6096cfdf661698cef42e05181dc64da05
Jaime Cervantes <jaime.cervantes.ve@gmail.com>
2025-05-15 16:23:59 -0600

    chore: add analytics script tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
-import "./styles/globals.css";
 import Header from "~/infrastructure/components/Header/Header";
+import "./styles/globals.css";
+import { GoogleAnalytics } from "@next/third-parties/google"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,17 +12,23 @@ export const metadata: Metadata = {
     "¿Como evitar enfermedades, ahorrar tiempo y dinero, al mismo tiempo que apoyas al medio ambiente y a tu comunidad?",
 };
 
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const gaId = process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS_ID;
+
   return (
     <html lang="es-MX">
       <body className={inter.className}>
         <Header />
         <main className="p-4">{children}</main>
       </body>
+      {process.env.NODE_ENV === 'production' && gaId && (
+        <GoogleAnalytics gaId={gaId} />
+      )}
     </html>
   );
 }
